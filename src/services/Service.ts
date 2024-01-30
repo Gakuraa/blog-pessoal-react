085b53b30cc3d0fb9cdad84dc01d033212989771
Gakuraa <gabrieliwakura924@gmail.com>
2024-01-30 17:44:05 -0300

    Commit como criação do perfil e implementação do toastAlert

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal-01uw.onrender.com/'
+  baseURL: 'https://blogpessoal-84of.onrender.com'
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
